a40e6aa7eee047f25add1bb1311a7ef680884a21
Alex <alejandro4703@hotmail.es>
2018-01-17 11:52:09 +0100

    Iniciado el repositorio de cadenas

---
+++ iesviergendelcarmen/cadena/teoria/Cadena1.java
@@ -0,0 +1,27 @@
+package com.iesviergendelcarmen.cadena.teoria;
+
+import java.util.Arrays;
+
+public class Cadena1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String cadena1 = "hola";
+		String cadena2 = "hola";
+		String cadena3 = new String ("hola");
+		//En este primer caso comparamos las direcciones de memoria 
+		System.out.println("cadena2 == cadena2: "+ (cadena1 == cadena2));
+		System.out.println("cadena1 == cadena3: " + (cadena1 == cadena3));
+		// En este segundo caso comparamos el contenido de las posiciones de memoria
+		System.out.println("contenido cadena2 == cadena2: "+ (cadena1.equals(cadena2)));
+		System.out.println("contenido cadena1 == cadena3: " + (cadena1.equalsIgnoreCase(cadena3)));
+		
+		//El metodo split elimina de la cadena la regla que introduzcas y genera cadenas nuevas cuando lo encuentra
+		//Este metodo retorna un vector 
+		String cadena4 = "hola mundo desde programacion java";
+		String [] cadenaDividida = cadena4.split(" ");
+		System.out.println(Arrays.toString(cadenaDividida));
+		
+	}
+
+}
