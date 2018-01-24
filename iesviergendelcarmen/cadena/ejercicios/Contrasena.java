51f751f2573cd82260a4731d5bc514919af83e48
Alex <alejandro4703@hotmail.es>
2018-01-24 12:29:19 +0100

    Añadidios ejercicios del dia 24 de enero

--- iesviergendelcarmen/cadena/ejercicios/Contrasena.java
+++ iesviergendelcarmen/cadena/ejercicios/Contrasena.java
@@ -7,12 +7,69 @@ public Contrasena(String contrasena) {
 	super();
 	this.contrasena = contrasena;
 }
+/*
  public boolean correcta () {
-	 return contrasena.matches(".*([A-Z]+[a-z]+[0-9]+).*");
+	 return contrasena.matches("^.*[a-z]+[A-Z]+.*$");
  }
+ */
+public boolean correcta1 () {
+	
+	return tiene8Caracteres() && tieneLetraMinuscula() && tieneLetraMayuscula() && tieneNumero() && tieneNoAlfnumerico();
+}
+private boolean tiene8Caracteres () {
+	return contrasena.length() >= 8;
+}
+private boolean tieneLetraMinuscula () {
+	boolean salir = false;
+	for (int i =0; i < contrasena.length(); i ++) {
+		if (Character.isLowerCase(contrasena.charAt(i))) {
+			salir = true;
+			break;
+		}
+	}
+	return salir || contrasena.contains("ñ");
+	//contrasena.matches("^.*[a-zñ]+.*$");
+}
+private boolean tieneLetraMayuscula(){
+	boolean salir = false;
+	for (int i =0; i < contrasena.length(); i ++) {
+		if (Character.isUpperCase(contrasena.charAt(i))) {
+			salir = true;
+			break;
+		}
+	}
+	return salir || contrasena.contains("Ñ");
+	//contrasena.matches("^.*[A-ZÑ]+.*$");
+}
+private boolean tieneNumero() {
+	boolean salir = false;
+	for (int i =0; i < contrasena.length(); i ++) {
+		if (Character.isDigit(contrasena.charAt(i))) {
+			salir = true;
+			break;
+		}
+	}
+	return salir || contrasena.contains("ñ");
+	//contrasena.matches("^.*[0-9]+.*$");
+	}
+private boolean tieneNoAlfnumerico () {
+	boolean salir = false;
+	for (int i =0; i < contrasena.length(); i ++) {
+		if (!Character.isLetterOrDigit(contrasena.charAt(i))) {
+			salir = true;
+			break;
+		}
+	}
+	return salir || contrasena.contains("ñ");
+	//contrasena.matches("^.*[\\W_]+.*$");
+}
 @Override
 public String toString() {
-	return "Contraseña [contrasena=" + contrasena + ", correcta()=" + correcta() + "]";
+	return "Contrasena [contrasena=" + contrasena + ", correcta1()=" + correcta1() + ", tiene8Caracteres()="
+			+ tiene8Caracteres() + ", tieneLetraMinuscula()=" + tieneLetraMinuscula() + ", tieneLetraMayuscula()="
+			+ tieneLetraMayuscula() + ", tieneNumero()=" + tieneNumero() + ", tieneNoAlfnumerico()="
+			+ tieneNoAlfnumerico() + "]";
 }
- 
+
+
 }
--- iesviergendelcarmen/cadena/ejercicios/TestContaseña.java
+++ iesviergendelcarmen/cadena/ejercicios/TestContaseña.java
@@ -6,11 +6,34 @@ public class TestContaseña {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Introduxca la contraseña");
+		/*System.out.println("Introduxca la contraseña");
 		Scanner in = new Scanner (System.in);
 		Contrasena contrasena = new Contrasena (in.next());
 		in.close();
 		System.out.println(contrasena);
+		*/
+		final String FUENTE_CARACTERES	 = "aAbBcCdDeEfFgGhHiIjJkKlLm-\n" + 
+				"MnN ̃n\n" + 
+				" ̃\n" + 
+				"NoOpPqQrRsStTuUvVwWxXyYzZ0123456789¿?()=@.:,;!¡&\n" + 
+				"{}\n" + 
+				"";
+		System.out.println("tamaño de fuente de caracteres: " +FUENTE_CARACTERES.length());
+	//generar el nº de claves a crear de 0-999
+		//int numeroDeClaves = (int) (Math.random() * 1000); // Si se quisiera llegar hasta 1000 habria que multiplciarlo por 1001
+		for (int i = 0; i < 1000; i++) {
+			int tamanoClave = (int) (Math.random() * FUENTE_CARACTERES.length());
+			System.out.println(tamanoClave);
+			Contrasena contrasena = null;
+			 String SContrasena = "";
+			for(int j=0 ; j < tamanoClave; j ++) {
+				int posicion = (int) (Math.random() * FUENTE_CARACTERES.length());
+				SContrasena += FUENTE_CARACTERES.charAt(posicion);
+			}
+			
+			System.out.println(SContrasena);
+			contrasena = new Contrasena (SContrasena);
+		}
 	}
 
 }
